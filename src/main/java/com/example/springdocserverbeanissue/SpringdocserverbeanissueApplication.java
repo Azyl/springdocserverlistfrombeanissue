afc1dbd252825cbe7ef23cecd6fcc5c08dc21bf4
Azyl <tataru.andrei.emanuel@gmail.com>
2019-09-21 20:22:43 +0300

    first commit

---
+++ src/main/java/com/example/springdocserverbeanissue/SpringdocserverbeanissueApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocserverbeanissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocserverbeanissueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocserverbeanissueApplication.class, args);
+    }
+
+}
